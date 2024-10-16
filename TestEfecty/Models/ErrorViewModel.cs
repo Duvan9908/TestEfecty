84d6c0712e2928a7579c940d9bea98553f160c0b
duvan9908 <duvanrodriguez@usantotomas.edu.co>
2024-10-16 09:25:12 -0500

    Primer Commit

---
+++ TestEfecty/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestEfecty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
